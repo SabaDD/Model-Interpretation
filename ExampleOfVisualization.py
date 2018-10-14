c227f2d3ed245e7997f9cc04f8a1efec18dae991
Wenshan He <whe@radimgws115.RADIMG>
2018-10-14 19:58:34 -0400

    some changes

--- ExampleOfVisualization.py
+++ ExampleOfVisualization.py
@@ -8,9 +8,18 @@ Created on Fri Sep  7 11:32:05 2018
 from keras.applications import ResNet50
 from vis.utils import utils
 from keras import activations
+from medpy.io import load
+import SimpleITK as sitk
+import os
 
-model = ResNet50(weights='imagenet', include_top= True)
+#model = ResNet50(weights='imagenet', include_top= True)
+#
+#model.summary()
+#
+#layer_idx = utils.find_layer_idx(model)
+#Read image using simpleITK
+PATH = os.getcwd()
+itkimage = sitk.ReadImage(PATH+'\BARP0807\LCC.mhd')
 
-model.summary()
-
-layer_idx = utils.find_layer_idx(model)
+#Convert the image into numpy array and then shuffle the dimentions to get axis in the order z, y, x
+mamograph = sitk.GetArrayFromImage(itkimage)
